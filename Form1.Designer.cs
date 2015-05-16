f288743394a130229cb61a26fe2f04b816f10224
zahidulAlam <zahid.plabon@gmail.com>
2015-05-16 17:21:12 +0600

    1st Ver.

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace YearlyCostCalculator
+{
+    partial class yearlyCostCalculatorUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.yearlyCostGroupBox = new System.Windows.Forms.GroupBox();
+            this.discountCheckBox = new System.Windows.Forms.CheckBox();
+            this.classComboBox = new System.Windows.Forms.ComboBox();
+            this.calculateTotalAmountButton = new System.Windows.Forms.Button();
+            this.selectClassLabelo = new System.Windows.Forms.Label();
+            this.yearlyCostGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // yearlyCostGroupBox
+            // 
+            this.yearlyCostGroupBox.Controls.Add(this.discountCheckBox);
+            this.yearlyCostGroupBox.Controls.Add(this.classComboBox);
+            this.yearlyCostGroupBox.Controls.Add(this.calculateTotalAmountButton);
+            this.yearlyCostGroupBox.Controls.Add(this.selectClassLabelo);
+            this.yearlyCostGroupBox.Location = new System.Drawing.Point(12, 23);
+            this.yearlyCostGroupBox.Name = "yearlyCostGroupBox";
+            this.yearlyCostGroupBox.Size = new System.Drawing.Size(455, 254);
+            this.yearlyCostGroupBox.TabIndex = 0;
+            this.yearlyCostGroupBox.TabStop = false;
+            this.yearlyCostGroupBox.Text = "Calculator for yearly cost of a student";
+            // 
+            // discountCheckBox
+            // 
+            this.discountCheckBox.AutoSize = true;
+            this.discountCheckBox.Location = new System.Drawing.Point(137, 132);
+            this.discountCheckBox.Name = "discountCheckBox";
+            this.discountCheckBox.Size = new System.Drawing.Size(192, 17);
+            this.discountCheckBox.TabIndex = 4;
+            this.discountCheckBox.Text = "Get 5% discount for early admission";
+            this.discountCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // classComboBox
+            // 
+            this.classComboBox.FormattingEnabled = true;
+            this.classComboBox.Location = new System.Drawing.Point(168, 73);
+            this.classComboBox.Name = "classComboBox";
+            this.classComboBox.Size = new System.Drawing.Size(161, 21);
+            this.classComboBox.TabIndex = 3;
+            // 
+            // calculateTotalAmountButton
+            // 
+            this.calculateTotalAmountButton.Location = new System.Drawing.Point(104, 181);
+            this.calculateTotalAmountButton.Name = "calculateTotalAmountButton";
+            this.calculateTotalAmountButton.Size = new System.Drawing.Size(225, 53);
+            this.calculateTotalAmountButton.TabIndex = 2;
+            this.calculateTotalAmountButton.Text = "Calculate total amount for one year";
+            this.calculateTotalAmountButton.UseVisualStyleBackColor = true;
+            this.calculateTotalAmountButton.Click += new System.EventHandler(this.calculateTotalAmountButton_Click);
+            // 
+            // selectClassLabelo
+            // 
+            this.selectClassLabelo.AutoSize = true;
+            this.selectClassLabelo.Location = new System.Drawing.Point(75, 73);
+            this.selectClassLabelo.Name = "selectClassLabelo";
+            this.selectClassLabelo.Size = new System.Drawing.Size(65, 13);
+            this.selectClassLabelo.TabIndex = 0;
+            this.selectClassLabelo.Text = "Select Class";
+            // 
+            // yearlyCostCalculatorUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 298);
+            this.Controls.Add(this.yearlyCostGroupBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "yearlyCostCalculatorUI";
+            this.Text = "Yearly Cost Calculator";
+            this.Load += new System.EventHandler(this.yearlyCostCalculatorUI_Load);
+            this.yearlyCostGroupBox.ResumeLayout(false);
+            this.yearlyCostGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox yearlyCostGroupBox;
+        private System.Windows.Forms.CheckBox discountCheckBox;
+        private System.Windows.Forms.ComboBox classComboBox;
+        private System.Windows.Forms.Button calculateTotalAmountButton;
+        private System.Windows.Forms.Label selectClassLabelo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YearlyCostCalculator
+{
+    public partial class yearlyCostCalculatorUI : Form
+    {
+        public yearlyCostCalculatorUI()
+        {
+            InitializeComponent();
+        }
+
+        private void yearlyCostCalculatorUI_Load(object sender, EventArgs e)
+        {
+            classComboBox.Items.Add("Class 1");
+            classComboBox.Items.Add("Class 2");
+            classComboBox.Items.Add("Class 3");
+            classComboBox.Items.Add("Class 4");
+            classComboBox.Items.Add("Class 5");
+            classComboBox.Items.Add("Class 6");
+            classComboBox.Items.Add("Class 7");
+            classComboBox.Items.Add("Class 8");
+            classComboBox.Items.Add("Class 9");
+            classComboBox.Items.Add("Class 10");
+            classComboBox.Items.Add("Class 11");
+            classComboBox.Items.Add("Class 12");
+        }
+
+        private void calculateTotalAmountButton_Click(object sender, EventArgs e)
+        {
+            string classSelect = classComboBox.Text;
+            if (classSelect == "Class 1" || classSelect == "Class 2" || classSelect == "Class 3" ||
+                classSelect == "Class 4" || classSelect == "Class 5")
+            {
+                double totalAmount = classSelectedFromOneToFive(classSelect);
+                if (discountCheckBox.Checked == true)
+                {
+                    double discount = (totalAmount*.05);
+                    totalAmount -= discount;
+                  
+                    MessageBox.Show("Total Amount is : " + totalAmount + " Tk");
+                }
+                else
+                {
+                    MessageBox.Show("Total Amount is : " + totalAmount + " Tk");
+                }
+
+            }
+            else if (classSelect == "Class 6" || classSelect == "Class 7" || classSelect == "Class 8")
+            {
+                double totalAmount = classSelectedFromSixToEight(classSelect);
+                if (discountCheckBox.Checked == true)
+                {
+                    double discount = (totalAmount*.05);
+                    totalAmount -= discount;
+                    MessageBox.Show("Total Amount is : " + totalAmount + " Tk");
+                }
+                else
+                {
+                    MessageBox.Show("Total Amount is : " + totalAmount + " Tk");
+                }
+            }
+            else
+            {
+                double totalAmount = classSelectedFromNineToTewelve(classSelect);
+                if (discountCheckBox.Checked == true)
+                {
+                    double discount = (totalAmount * .05);
+                    totalAmount -= discount;
+                    MessageBox.Show("Total Amount is : " + totalAmount + " Tk");
+                }
+                else
+                {
+                    MessageBox.Show("Total Amount is : " + totalAmount + " Tk");
+                }
+            }
+        }
+
+        static double classSelectedFromOneToFive(string classType)
+        {
+            if (classType == "Class 1" || classType == "Class 2")
+            {
+                return 10000.0 + (12.0*500.0);
+            }
+            else
+            {
+                return 10000.0 + (12.0 * 1000.0);   
+            }
+        }
+
+        static double classSelectedFromSixToEight(string classType)
+        {
+            return 12000.0 + (12.0 * 1500.0);
+        }
+
+        static double classSelectedFromNineToTewelve(string classType)
+        {
+            if (classType == "Class 9" || classType == "Class 10")
+            {
+                return 15000.0 + (12.0 * 2000.0);
+            }
+            else
+            {
+                return 15000.0 + (12.0 * 2700.0);
+            }
+        }
+
+
+
+    
+    }
+}
